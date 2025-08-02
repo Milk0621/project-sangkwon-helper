ec825c3951fabd728785ed30b97c9963dc438385
Milk0621 <wltjs5360@gmail.com>
2025-08-02 21:17:16 +0900

    style: 헤더 css 수정

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,21 +1,22 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import './Header.css';
+import styles from './Header.module.css';
 
 function Header() {
     return(
-        <Navbar bg="light" data-bs-theme="light">
-            <Container>
-                <Navbar.Brand as={Link} to="/">Sangkwon Helper</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link as={Link} to="/">홈</Nav.Link>
-                    <Nav.Link as={Link} to="/analysis">상권분석</Nav.Link>
-                </Nav>
-                <Nav>
-                    <Nav.Link as={Link} to="/login">로그인</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <Link to="/">상권도우미</Link>
+            </div>
+            <nav className={styles.nav}>
+                <Link to="/">홈</Link>
+                <Link to="/analysis">상권 분석</Link>
+                <Link to="/map">지도 보기</Link>
+                <Link to="/guide">이용 가이드</Link>
+            </nav>
+            <nav className={styles.nav}>
+                <Link to="/login">로그인</Link>
+            </nav>
+        </header>
     )
 }
 
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
-import style from './Home.module.css';
+import styles from './Home.module.css';
 
 function Home() {
     return(
         <div>
-            <div className={style.mainBanner}>
+            <div className={styles.mainBanner}>
                 <h1>지역 창업을 위한 상권 분석 플랫폼</h1>
                 <p>지금 바로 나에게 맞는 위치를 찾아보세요!</p>
                 <button>분석 시작하기</button>
