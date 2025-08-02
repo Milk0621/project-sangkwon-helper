b0b099b7efc9d3812f68e418ffbf8f600ca7eb28
Milk0621 <wltjs5360@gmail.com>
2025-08-03 01:14:12 +0900

    refactor: Link 경로 수정 및 css 여백 수정

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,18 +4,18 @@ import styles from './Header.module.css';
 function Header() {
     return(
         <header className={styles.header}>
-            <div className={styles.logo}>
-                <Link to="/">상권도우미</Link>
-            </div>
-            <nav className={styles.nav}>
-                <Link to="/">홈</Link>
-                <Link to="/analysis">상권 분석</Link>
-                <Link to="/map">지도 보기</Link>
-                <Link to="/guide">이용 가이드</Link>
-            </nav>
-            <nav className={styles.nav}>
-                <Link to="/login">로그인</Link>
-            </nav>
+                <div className={styles.logo}>
+                    <Link to="/">상권도우미</Link>
+                </div>
+                <nav className={styles.nav}>
+                    <Link to="/">홈</Link>
+                    <Link to="/analysis">상권 분석</Link>
+                    <Link to="/map">지도 보기</Link>
+                    <Link to="/guide">이용 가이드</Link>
+                </nav>
+                <nav className={styles.nav}>
+                    <Link to="/auth">로그인</Link>
+                </nav>
         </header>
     )
 }
