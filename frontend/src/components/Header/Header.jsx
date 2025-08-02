13857317994899f04de2d8553beeddb936cb4f3f
Milk0621 <wltjs5360@gmail.com>
2025-08-02 19:48:47 +0900

    style: Header 컴포넌트 css 구현

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,16 +1,19 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 function Header() {
     return(
         <Navbar bg="light" data-bs-theme="light">
             <Container>
-            <Navbar.Brand as={Link} to="/">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
-                <Nav.Link as={Link} to="/">홈</Nav.Link>
-                <Nav.Link as={Link} to="/analysis">상권분석</Nav.Link>
-                <Nav.Link as={Link} to="/login">로그인</Nav.Link>
-            </Nav>
+                <Navbar.Brand as={Link} to="/">Sangkwon Helper</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link as={Link} to="/">홈</Nav.Link>
+                    <Nav.Link as={Link} to="/analysis">상권분석</Nav.Link>
+                </Nav>
+                <Nav>
+                    <Nav.Link as={Link} to="/login">로그인</Nav.Link>
+                </Nav>
             </Container>
         </Navbar>
     )
