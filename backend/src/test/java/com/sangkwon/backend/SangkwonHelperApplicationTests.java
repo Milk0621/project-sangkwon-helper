88739da13bfa01288b5e2075a354d078229179a0
Milk0621 <wltjs5360@gmail.com>
2025-08-02 14:39:04 +0900

    feat: 백엔드 기본 구조 추가

---
+++ backend/src/test/java/com/sangkwon/backend/SangkwonHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangkwon.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangkwonHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
