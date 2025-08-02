6dc595f61d7798315da57b491bb52c2d59271d77
Milk0621 <wltjs5360@gmail.com>
2025-08-03 01:14:29 +0900

    feat: 새로운 auth 라우트 경로 추가

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Layout from '../components/Layout/Layout';
 import Home from '../pages/Home/Home';
+import Auth from '../components/Auth/Auth';
 
 function AppRouter() {
     return (
@@ -8,6 +9,7 @@ function AppRouter() {
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
+                    <Route path="/auth" element={<Auth />} />
                 </Route>
             </Routes>
         </BrowserRouter>
