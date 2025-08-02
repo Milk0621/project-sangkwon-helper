1ca26c3551d018e9092d83c746f15a1076a214a5
Milk0621 <wltjs5360@gmail.com>
2025-08-02 18:47:56 +0900

    feat: AppRouter 컴포넌트 생성 및 라우팅 기본 구조 설정

---
+++ frontend/src/routes/AppRouter.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Layout from '../components/Layout/Layout';
+import Home from '../pages/Home/Home';
+
+function AppRouter() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRouter;
