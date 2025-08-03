e594e3341a6fee635e99b599e0d15b74dcd39ffc
Milk0621 <wltjs5360@gmail.com>
2025-08-03 16:33:02 +0900

    style: Footer 구조 변경 및 css 수정

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,41 +4,43 @@ function Footer() {
     return(
         <footer>
             <div>
-                <div>
-                    <div className={styles.logo}>
-                        <div className={styles.kor}>
-                            상권<span className={styles.blue}>도우미</span>
+                <div className={styles.gridDiv}>
+                    <div>
+                        <div className={styles.logo}>
+                            <div className={styles.kor}>
+                                상권<span className={styles.blue}>도우미</span>
+                            </div>
+                            <div className={styles.eng}>COMMERCIAL HELPER</div>
+                            <div>지역 상권분석 플랫폼으로 성공적인 <br/> 창업을 도와드립니다</div>
                         </div>
-                        <div className={styles.eng}>COMMERCIAL HELPER</div>
-                        <div>지역 상권분석 플랫폼으로 성공적인 <br/> 창업을 도와드립니다</div>
                     </div>
-                </div>
-                <div>
-                    <h5>서비스</h5>
-                    <ul>
-                        <li>상권 분석</li>
-                        <li>유동인구 분석</li>
-                        <li>경쟁업체 분석</li>
-                        <li>위치 추천</li>
-                    </ul>
-                </div>
-                <div>
-                    <h5>회사</h5>
-                    <ul>
-                        <li>회사 소개</li>
-                        <li>팀</li>
-                        <li>채용정보</li>
-                        <li>투자정보</li>
-                    </ul>
-                </div>
-                <div>
-                    <h5>고객지원</h5>
-                    <ul>
-                        <li>이용가이드</li>
-                        <li>FAQ</li>
-                        <li>문의하기</li>
-                        <li>개발자 API</li>
-                    </ul>
+                    <div>
+                        <h5>서비스</h5>
+                        <ul>
+                            <li>상권 분석</li>
+                            <li>유동인구 분석</li>
+                            <li>경쟁업체 분석</li>
+                            <li>위치 추천</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h5>회사</h5>
+                        <ul>
+                            <li>회사 소개</li>
+                            <li>팀</li>
+                            <li>채용정보</li>
+                            <li>투자정보</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h5>고객지원</h5>
+                        <ul>
+                            <li>이용가이드</li>
+                            <li>FAQ</li>
+                            <li>문의하기</li>
+                            <li>개발자 API</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div className={styles.footerBottom}>
