702beab713ec02f286b2719c9dee99c7bbcd4431
Milk0621 <wltjs5360@gmail.com>
2025-08-05 00:39:43 +0900

    feat: 회원가입 시 이메일과 이름 중복에 대한 예외 처리 추가

--- backend/src/main/java/com/sangkwon/backend/domain/users/controller/UsersController.java
+++ backend/src/main/java/com/sangkwon/backend/domain/users/controller/UsersController.java
@@ -1,6 +1,7 @@
 package com.sangkwon.backend.domain.users.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +21,21 @@ public class UsersController {
 	
 	@PostMapping("/auth/register")
 	public ResponseEntity<?> register(@RequestBody UserRegisterRequestDTO requestDTO) {
-		usersSerivce.register(requestDTO);
-		return ResponseEntity.status(HttpStatus.CREATED).body("회원가입 성공");
+		try {
+			usersSerivce.register(requestDTO);
+			return ResponseEntity.status(HttpStatus.CREATED).body("회원가입 성공");
+		} catch (DataIntegrityViolationException e) {
+			String message = e.getRootCause().getMessage(); // MySQL 에러 메시지 추출
+
+	        if (message.contains("email")) {
+	            return ResponseEntity.status(HttpStatus.CONFLICT).body("이미 사용 중인 이메일입니다.");
+	        } else if (message.contains("name")) {
+	            return ResponseEntity.status(HttpStatus.CONFLICT).body("이미 사용 중인 이름입니다.");
+	        } else {
+	            return ResponseEntity.status(HttpStatus.CONFLICT).body("중복된 항목이 있습니다.");
+	        }
+		} catch (Exception e) {
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("회원가입 중 오류 발생");
+	    }
 	}
 }
