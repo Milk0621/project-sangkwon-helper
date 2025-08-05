1122d5a82fb96d5f3be79ec5485a5f93522cbc2a
Milk0621 <wltjs5360@gmail.com>
2025-08-05 16:40:32 +0900

    feat: Spring Security 기본 설정 및 비밀번호 인코더 등록 및 cofig -> config 패키지명 수정

--- backend/src/main/java/com/sangkwon/backend/config/WebConfig.java
+++ backend/src/main/java/com/sangkwon/backend/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.sangkwon.backend.cofig;
+package com.sangkwon.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
