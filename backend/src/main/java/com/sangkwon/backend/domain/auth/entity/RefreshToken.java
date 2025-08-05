21346da2a47f0b069219dd539245e47a010e817a
Milk0621 <wltjs5360@gmail.com>
2025-08-05 16:39:22 +0900

    feat: RefreshToken 엔티티 클래스 생성

---
+++ backend/src/main/java/com/sangkwon/backend/domain/auth/entity/RefreshToken.java
@@ -0,0 +1,54 @@
+package com.sangkwon.backend.domain.auth.entity;
+
+import java.time.LocalDateTime;
+
+public class RefreshToken {
+	private int id;
+	private String email;
+	private String token;
+	private LocalDateTime expiresAt;
+	private LocalDateTime createdAt;
+	
+	public RefreshToken() {}
+	
+	public RefreshToken(int id, String email, String token, LocalDateTime expiresAt, LocalDateTime createdAt) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.token = token;
+		this.expiresAt = expiresAt;
+		this.createdAt = createdAt;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public LocalDateTime getExpiresAt() {
+		return expiresAt;
+	}
+	public void setExpiresAt(LocalDateTime expiresAt) {
+		this.expiresAt = expiresAt;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+}
