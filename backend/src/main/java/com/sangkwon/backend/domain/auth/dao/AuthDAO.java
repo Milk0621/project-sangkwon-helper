f9711f3524d9610efa2bed5f50f339929287b67a
Milk0621 <wltjs5360@gmail.com>
2025-08-05 16:41:43 +0900

    feat: RefreshToken 관리를 위한 MyBatisAuthDAO 구현

---
+++ backend/src/main/java/com/sangkwon/backend/domain/auth/dao/AuthDAO.java
@@ -0,0 +1,11 @@
+package com.sangkwon.backend.domain.auth.dao;
+
+import com.sangkwon.backend.domain.auth.entity.RefreshToken;
+
+public interface AuthDAO {
+	RefreshToken findByEmail(String email);
+
+    void insertToken(RefreshToken refreshToken);
+
+    void updateToken(RefreshToken refreshToken);
+}
---
+++ backend/src/main/java/com/sangkwon/backend/domain/auth/dao/mybatis/MyBatisAuthDAO.java
@@ -0,0 +1,34 @@
+package com.sangkwon.backend.domain.auth.dao.mybatis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.sangkwon.backend.domain.auth.dao.AuthDAO;
+import com.sangkwon.backend.domain.auth.entity.RefreshToken;
+import com.sangkwon.backend.domain.auth.mapper.mybatis.RefreshTokenMapper;
+
+@Repository
+public class MyBatisAuthDAO implements AuthDAO {
+	private final RefreshTokenMapper refreshTokenMapper;
+
+	@Autowired
+	public MyBatisAuthDAO(RefreshTokenMapper refreshTokenMapper) {
+		this.refreshTokenMapper = refreshTokenMapper;
+	}
+
+	@Override
+	public RefreshToken findByEmail(String email) {
+		return refreshTokenMapper.findByEmail(email);
+	}
+
+	@Override
+	public void insertToken(RefreshToken refreshToken) {
+		refreshTokenMapper.insertToken(refreshToken);
+	}
+
+	@Override
+	public void updateToken(RefreshToken refreshToken) {
+		refreshTokenMapper.updateToken(refreshToken);
+	}
+		
+}
