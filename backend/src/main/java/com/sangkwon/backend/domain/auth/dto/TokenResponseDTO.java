99ae7586034df07734c81b611530834f44f1bcbb
Milk0621 <wltjs5360@gmail.com>
2025-08-05 16:38:54 +0900

    feat: 로그인 요청 및 응답을 위한 DTO 클래스 구현

---
+++ backend/src/main/java/com/sangkwon/backend/domain/auth/dto/TokenResponseDTO.java
@@ -0,0 +1,24 @@
+package com.sangkwon.backend.domain.auth.dto;
+
+public class TokenResponseDTO {
+	private String accessToken;
+	private String refreshToken;
+	
+	public TokenResponseDTO(String accessToken, String refreshToken) {
+		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
+	}
+	
+	public String getAccessToken() {
+		return accessToken;
+	}
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+	public String getRefreshToken() {
+		return refreshToken;
+	}
+	public void setRefreshToken(String refreshToken) {
+		this.refreshToken = refreshToken;
+	}
+}
