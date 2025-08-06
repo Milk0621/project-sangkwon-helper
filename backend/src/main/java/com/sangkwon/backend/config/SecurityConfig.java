0eac6e54fdba583187f0d9c192029162ebb41d6f
Milk0621 <wltjs5360@gmail.com>
2025-08-06 17:56:34 +0900

    feat: JWT 인증 필터 등록 및 인증 엔드포인트 허용 설정

--- backend/src/main/java/com/sangkwon/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/sangkwon/backend/config/SecurityConfig.java
@@ -1,21 +1,38 @@
 package com.sangkwon.backend.config;
-
+import com.sangkwon.backend.domain.auth.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.sangkwon.backend.domain.auth.dao.AuthDAO;
+import com.sangkwon.backend.domain.auth.jwt.JwtAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthDAO authDAO;
+
+    SecurityConfig(JwtTokenProvider jwtTokenProvider, AuthDAO authDAO) {
+        this.jwtTokenProvider = jwtTokenProvider;
+		this.authDAO = authDAO;
+    }
 	
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
-                .authorizeHttpRequests(auth -> auth
-                    .anyRequest().permitAll() // 모든 요청 허용
-                )
-                .build();
+		return http
+		    .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+		    .authorizeHttpRequests(auth -> auth
+				.requestMatchers("/api/auth/**").permitAll() // 인증 없이 허용할 엔드포인트
+		        .anyRequest().authenticated()
+		    )
+		    .addFilterBefore(
+	            new JwtAuthenticationFilter(jwtTokenProvider, authDAO), // 만든 필터 등록
+	            UsernamePasswordAuthenticationFilter.class
+	        )
+	        .build();
     }
 	
 }
