b51ab43aaa29b1811b4082855d87e24b3098c982
Milk0621 <wltjs5360@gmail.com>
2025-08-07 16:24:51 +0900

    feat: Axios 인스턴스에 쿠키 자동 포함 설정 추가

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: `http://localhost:8080/api`
+    baseURL: `http://localhost:8080/api`,
+    withCredentials: true   // 쿠키 자동 포함
 });
 
 api.interceptors.request.use((config)=>{
