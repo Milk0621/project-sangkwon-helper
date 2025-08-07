f9620dd22f14cf4d98edf2337c893bf43facceec
Milk0621 <wltjs5360@gmail.com>
2025-08-07 17:30:04 +0900

    feat: 현재 로그인된 사용자 정보 조회 API 구현

--- backend/src/main/java/com/sangkwon/backend/domain/users/controller/UsersController.java
+++ backend/src/main/java/com/sangkwon/backend/domain/users/controller/UsersController.java
@@ -1,16 +1,33 @@
 package com.sangkwon.backend.domain.users.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sangkwon.backend.domain.auth.jwt.JwtTokenProvider;
+import com.sangkwon.backend.domain.users.entity.Users;
 import com.sangkwon.backend.domain.users.service.UsersService;
 
 @RestController
 @RequestMapping("/api/users")
 public class UsersController {
 	
-	@Autowired
-	private UsersService usersSerivce;
+	private UsersService usersService;
+	private final JwtTokenProvider jwtTokenProvider;
 	
+	public UsersController(UsersService usersService, JwtTokenProvider jwtTokenProvider) {
+		this.usersService = usersService;
+		this.jwtTokenProvider = jwtTokenProvider;
+	}
+	
+	@GetMapping("/me")
+	public ResponseEntity<Users> getCurrentUser(@RequestHeader("Authorization") String authHeader) {
+		String token = authHeader.replace("Bearer ", "");
+		String email = jwtTokenProvider.getUserEmailFromToken(token);
+		
+		Users user = usersService.findByEmail(email);
+		return ResponseEntity.ok(user);
+	}
 }
