c88b973ca7837ff3ef0a390a1f0ca6cec6bbd2fe
Milk0621 <wltjs5360@gmail.com>
2025-08-07 17:30:31 +0900

    feat: 이메일로 사용자 정보를 조회하는 서비스 메서드 구현

--- backend/src/main/java/com/sangkwon/backend/domain/users/service/UsersService.java
+++ backend/src/main/java/com/sangkwon/backend/domain/users/service/UsersService.java
@@ -1,5 +1,9 @@
 package com.sangkwon.backend.domain.users.service;
 
+import com.sangkwon.backend.domain.users.entity.Users;
+
 public interface UsersService {
+
+	Users findByEmail(String email);
 	
 }
--- backend/src/main/java/com/sangkwon/backend/domain/users/service/UsersServiceImpl.java
+++ backend/src/main/java/com/sangkwon/backend/domain/users/service/UsersServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.sangkwon.backend.domain.users.dao.UsersDAO;
+import com.sangkwon.backend.domain.users.entity.Users;
 
 @Service
 public class UsersServiceImpl implements UsersService {
@@ -14,6 +15,11 @@ public class UsersServiceImpl implements UsersService {
 	@Autowired
 	public UsersServiceImpl(@Qualifier("myBatisUsersDAO") UsersDAO usersDAO) {
 		this.usersDAO = usersDAO;
+	}
+
+	@Override
+	public Users findByEmail(String email) {
+		return usersDAO.findByEmail(email);
 	}
 	
 }
