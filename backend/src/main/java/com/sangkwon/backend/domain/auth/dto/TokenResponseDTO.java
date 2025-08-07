945113739485dbc03071948f0e3cb96da0092318
Milk0621 <wltjs5360@gmail.com>
2025-08-07 16:23:49 +0900

    feat: TokenResponseDTO에 accessToken만 받는 생성자 추가

--- backend/src/main/java/com/sangkwon/backend/domain/auth/dto/TokenResponseDTO.java
+++ backend/src/main/java/com/sangkwon/backend/domain/auth/dto/TokenResponseDTO.java
@@ -3,6 +3,10 @@ package com.sangkwon.backend.domain.auth.dto;
 public class TokenResponseDTO {
 	private String accessToken;
 	private String refreshToken;
+
+	public TokenResponseDTO(String accessToken) {
+		this.accessToken = accessToken;
+	}
 	
 	public TokenResponseDTO(String accessToken, String refreshToken) {
 		this.accessToken = accessToken;
