b5f142cea229e5b984e03fcb11f55aefe217bfcc
Milk0621 <wltjs5360@gmail.com>
2025-08-12 22:12:16 +0900

    feat: 지도 페이지에 PrivateRoute 적용하여 로그인 사용자만 접근 가능하도록 수정

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -3,6 +3,7 @@ import Layout from '../components/Layout/Layout';
 import Home from '../pages/Home/Home';
 import Auth from '../components/Auth/Auth';
 import Map from '../pages/Map/Map';
+import PrivateRoute from '../Auth/PrivateRoute';
 
 function AppRouter() {
     return (
@@ -11,7 +12,10 @@ function AppRouter() {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
                     <Route path="/auth" element={<Auth />} />
-                    <Route path="/map" element={<Map />} />
+                    
+                    <Route element={<PrivateRoute />}>
+                        <Route path="/map" element={<Map />} />
+                    </Route>
                 </Route>
             </Routes>
         </BrowserRouter>
