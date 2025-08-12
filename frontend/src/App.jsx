22fb9b3ff373acc71573ce8e332ed8c5b5b3c087
Milk0621 <wltjs5360@gmail.com>
2025-08-12 22:12:45 +0900

    feat: AuthProvider 추가하여 새로고침 시에도 로그인 상태 유지 기능 구현

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
+import AuthProvider from './Auth/AuthProvider';
 import AppRouter from './routes/AppRouter';
 
 function App() {
   return (
     <div className="App">
-      <AppRouter />
+      <AuthProvider>
+        <AppRouter />
+      </AuthProvider>
     </div>
   );
 }
