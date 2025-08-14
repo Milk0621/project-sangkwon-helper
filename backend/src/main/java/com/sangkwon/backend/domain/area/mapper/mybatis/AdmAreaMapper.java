b8a6229a38f17657993caf95340941dfd3deaa6f
Milk0621 <wltjs5360@gmail.com>
2025-08-14 23:15:19 +0900

    feat: 좌표 반환을 위한 MyBatis 매퍼 및 인터페이스 구현

--- backend/src/main/java/com/sangkwon/backend/domain/area/mapper/mybatis/AdmAreaMapper.java
+++ backend/src/main/java/com/sangkwon/backend/domain/area/mapper/mybatis/AdmAreaMapper.java
@@ -5,11 +5,15 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.sangkwon.backend.domain.area.dto.AdongCenterDTO;
 import com.sangkwon.backend.domain.area.dto.AreaCountDTO;
+import com.sangkwon.backend.domain.area.dto.SigunguCenterDTO;
 
 @Mapper
 public interface AdmAreaMapper {
 	List<AreaCountDTO> listSidoStats();
     List<AreaCountDTO> listSigunguStats(@Param("sido") String sido);
 	List<AreaCountDTO> listDongStats(@Param("sido") String sido, @Param("sigungu") String sigungu);
+	SigunguCenterDTO getSigunguCenter(@Param("sido") String sido, @Param("sigungu") String sigungu);
+	AdongCenterDTO getDongCenter(@Param("sido") String sido, @Param("sigungu") String sigungu, @Param("dong") String dong);
 }
